f845e71a606a58fa21425ffe0e4beae6fd260986
Dogukan Ulu <dogukannulu@gmail.com>
2023-06-14 02:04:13 +0300

    add dags, modify scripts

--- crypto_data_stream.py
+++ crypto_data_stream.py
@@ -1,76 +1,22 @@
 import time
-import logging
 import requests
 import json
 import os
 from kafka import KafkaProducer
 
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s: %(funcName)s: %(levelname)s: %(message)s"
-)
-logger = logging.getLogger("crypto_api_streaming")
-api_key = os.environ.get("COINMARKETCAP_API_KEY")
-
-class DataConfig:
-    """
-    Obtains the global variables from config.json
-    """
-
-    CONFIG_FILE = "config.json"  # Path to the configuration file
-
-    @classmethod
-    def load_config(cls):
-        """
-        Loads the configuration from the JSON file.
-        """
-        try:
-            with open(cls.CONFIG_FILE, "r") as f:
-                config = json.load(f)
-            return config
-        except Exception as e:
-            logger.exception(
-                f"Failed to load configuration from {cls.CONFIG_FILE}: {e}"
-            )
-            raise
-
-    @classmethod
-    def get_url(cls):
-        """
-        Returns the url from the configuration.
-        """
-        config = cls.load_config()
-        return config.get("url", {})
-
-    @classmethod
-    def get_headers(cls):
-        """
-        Returns the headers from the configuration.
-        """
-        config = cls.load_config()
-        return config.get("headers", {})
-
-    @classmethod
-    def get_parameters(cls):
-        """
-        Returns the parameters from the configuration.
-        """
-        config = cls.load_config()
-        return config.get("parameters", {})
-
 
 def data_stream():
-    try:
-        # These are the Kafka brokers defined in docker-compose-kafka.yml
-        producer = KafkaProducer(bootstrap_servers=['kafka1:19092', 'kafka2:19093', 'kafka3:19094'])    
-    except Exception as e:
-        logger.error(f'Cannot connect to Kafka producer due to: {e}')
     producer = KafkaProducer(bootstrap_servers=['kafka1:19092', 'kafka2:19093', 'kafka3:19094'])    
 
-    url = DataConfig.get_url()
-    parameters = DataConfig.get_parameters()
-    headers = DataConfig.get_headers()
-
-    headers["X-CMC_PRO_API_KEY"] = api_key
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = {
+        'symbol': 'BTC,ETH',
+        'convert': 'USD'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': "5620aaf5-fcb8-4295-8612-3a4528f938cc"
+    }
 
     response = requests.get(url, headers=headers, params=parameters)
     data = json.loads(response.text)
---
+++ read_kafka_write_mysql.py
@@ -0,0 +1,77 @@
+import mysql.connector
+import logging
+import json
+import time
+from kafka import KafkaConsumer
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+logger = logging.getLogger("read_kafka_write_mysql")
+
+
+try:
+    conn = mysql.connector.connect(
+        host="mysql",
+        database="mysql",
+        user="mysql",
+        password="mysql",
+        port=3306
+    )
+    cur = conn.cursor()
+    logger.info('MySQL server connection is successful')
+except Exception as e:
+    logger.error(f"Couldn't create the MySQL connection due to: {e}")
+
+try:
+    consumer = KafkaConsumer(
+        'btc_prices',
+        bootstrap_servers=['kafka1:19092', 'kafka2:19093', 'kafka3:19094'],
+        auto_offset_reset='latest',  # Start consuming from the latest offset
+        enable_auto_commit=False  # Disable auto-commit to have manual control over offsets
+    )
+    logger.info("Kafka connection successful")
+except Exception as e:
+    logger.error(f"Kafka connection is not successsful. {e}")
+
+
+def create_new_tables_in_mysql():
+    """
+    Creates the btc_prices table in MySQL server.
+    """
+    try:
+        cur.execute("""CREATE TABLE IF NOT EXISTS btc_prices 
+        (timestamp VARCHAR(50), name VARCHAR(10), price FLOAT, volume_24h FLOAT, percent_change_24h FLOAT)""")
+        logging.info("Table created successfully in MySQL server")
+    except Exception as e:
+        logging.error(f'Tables cannot be created due to: {e}')
+
+
+def insert_data_into_mysql():
+    """
+    Insert the latest messages coming to Kafka consumer to MySQL table.
+    """
+    end_time = time.time() + 120 # the script will run for 2 minutes
+    for msg in consumer: 
+        kafka_message = json.loads(msg.value.decode('utf-8'))
+
+        if time.time() > end_time:
+            break
+
+        try:
+            # Create an SQL INSERT statement
+            insert_query = f"INSERT INTO btc_prices (timestamp, name, price, volume_24h, percent_change_24h) VALUES (%s, %s, %s, %s, %s)"
+            cur.execute(insert_query, (kafka_message['timestamp'], kafka_message['name'], kafka_message['price'], kafka_message['volume_24h'], kafka_message['percent_change_24h']))
+            conn.commit()
+        except Exception as e:
+            logging.error(f"Error inserting data: {e}")
+
+
+
+if __name__ == "__main__":
+    create_new_tables_in_mysql()
+    insert_data_into_mysql()
+
+    cur.close()
+    conn.close()
+
+
